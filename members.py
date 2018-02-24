8ac8d48f3ba57fc8dcc9913075a4ca8c76bb1318
Scott Means <smeans@gmail.com>
2018-02-24 17:20:26 -0500

    added delete support

--- members.py
+++ members.py
@@ -5,23 +5,35 @@ cn = sqlite3.connect('makermon.db')
 schema = {'members': ['name', 'fob_id']}
 
 cn.execute('CREATE TABLE IF NOT EXISTS members (%s)' % ', '.join(schema['members']))
+cn.commit()
 
 def escape(s):
     return s.replace("'", "''")
 
 def colnames(data):
-    return '(%s)' % ', '.join([escape(key) for key in data.keys()]);
+    return '%s' % ', '.join([escape(key) for key in data.keys()]);
 
 def valmask(data):
-    return ['?'] * len(data.keys())
+    return ', '.join(['?'] * len(data.keys()))
 
 def vallist(data):
     return tuple([data[key] for key in data.keys()])
 
 def members():
-    return []
+    c = cn.cursor()
+
+    c.execute('SELECT ROWID, * FROM members')
+    cols = ['member_id'] + schema['members']
+
+    members = [dict(zip(cols, row)) for row in c.fetchall()]
+
+    return members
 
 def add_member(data):
     sql = 'INSERT INTO members (%s) VALUES (%s)' % (colnames(data), valmask(data))
+    cn.execute(sql, vallist(data))
+    cn.commit()
 
-    #cn.execute(sql, vallist(data))
+def delete_member(member_id):
+    cn.execute('DELETE FROM members WHERE ROWID = ?', member_id)
+    cn.commit()
--- uimodules.py
+++ uimodules.py
@@ -6,10 +6,19 @@ class Members(tornado.web.UIModule):
     def add_member(handler):
         data = { k: handler.get_argument(k) for k in filter(lambda key: not key.startswith('_'), handler.request.arguments) }
         members.add_member(data)
-        
+
     add_member = staticmethod(add_member)
+
+    def delete_member(handler):
+        member_id = handler.get_argument('member_id')
+        members.delete_member(member_id)
+
+    delete_member = staticmethod(delete_member)
 
     def render(self):
         return self.render_string('module-members.html', members=members.members())
 
-service_actions = {'add_member': Members.add_member}
+service_actions = {
+    'add_member': Members.add_member,
+    'delete_member': Members.delete_member
+}
