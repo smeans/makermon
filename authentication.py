0fecf807779e6dac7e2529ec28c832bf56b6c6ea
Dallas Epperson <dallas.epperson@gmail.com>
2018-03-01 16:46:09 -0500

    Removed debug print statements

--- authentication.py
+++ authentication.py
@@ -7,15 +7,15 @@ class GoogleOAuth2LoginHandler(tornado.web.RequestHandler, tornado.auth.GoogleOA
     @tornado.gen.coroutine
     def get(self):
         if self.get_current_user():
-            print 'someone hit login page but they already logged in'
+            # 'someone hit login page but they already logged in'
             self.redirect('/')
             return
 
         if self.get_argument('code', False):
-            print 'someone hit login page with oauth2 token'
+            # 'someone hit login page with oauth2 token'
             user = yield self.get_authenticated_user(redirect_uri='http://localhost:8888/auth/login', code=self.get_argument('code'))
             if not user:
-                print 'not user'
+                # 'not user'
                 self.clear_all_cookies() 
                 raise tornado.web.HTTPError(500, 'Google authentication failed')
 
@@ -29,24 +29,23 @@ class GoogleOAuth2LoginHandler(tornado.web.RequestHandler, tornado.auth.GoogleOA
             #TODO: Determine if user['email'] exists in our member table. If so, auth'd. If not, not.
             #For now, this simple check:
             if not user['email'].split('@')[-1] == config.settings["oauth2"]["requiredDomain"]:
-                print 'User ' + user['email'] + ' is not authorized to use this application.'
+                # 'User ' + user['email'] + ' is not authorized to use this application.'
                 self.clear_all_cookies() 
                 self.redirect('/')
                 return
 
-            print 'User ' + user['email'] + ' logged in via OAuth2.'
+            # 'User ' + user['email'] + ' logged in via OAuth2.'
             self.set_secure_cookie(config.settings["authCookie"]["name"], user['email'], 1) 
             self.redirect('/')
             return
 
         elif self.get_secure_cookie(config.settings["authCookie"]["name"]):
-            print 'someone hit login page but already has auth session cookie'
-            print self.get_secure_cookie(config.settings["authCookie"]["name"])
+            # 'someone hit login page but already has auth session cookie'
             self.redirect('/')
             return
 
         else:
-            print 'someone hit login page directly'
+            # 'someone hit login page directly'
             yield self.authorize_redirect(
                 redirect_uri='http://localhost:8888/auth/login',
                 client_id=config.settings["oauth2"]["clientId"],
