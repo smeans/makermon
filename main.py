19fca763573ea8318e880c83985707c2ef87ebd2
drago3711 <drago3711@gmail.com>
2018-05-03 19:54:27 -0400

    Added twitter integration to the door scanner. Adds a new library dependancy Updated the readme with new dependancy info Created a pool of tweets to randomly pick from Created a cooldown for tweets to prevent spam messages

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python
 import os
 import sqlite3
 import tornado.ioloop
 import tornado.web
+import time
+import sys
+import random
 
 import database
 import uimodules
+import twitter
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -20,6 +25,8 @@ class MainHandler(tornado.web.RequestHandler):
 
 class PermHandler(tornado.web.RequestHandler):
     def get(self,permPath):
+        print self.request,  time.time(), time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
+        sys.stdout.flush()
         paths = permPath.split( '/' )
         cn = sqlite3.connect('makermon.db')
         c = cn.cursor()
@@ -29,6 +36,16 @@ class PermHandler(tornado.web.RequestHandler):
         self.set_status( 403 )
         if len( c.fetchall() ):
             self.set_status( 200 )
+            try:
+                curr_time = time.time()
+                if (curr_time - self.settings['twitter_last_tweet']) > self.settings['twitter_cooldown_sec']:
+                    tweet = random.choice(self.settings['twitter_tweet_pool'])
+                    print "tweeting:",tweet
+                    self.settings['twitter_api'].PostUpdate(tweet)
+                    self.settings['twitter_last_tweet'] = curr_time
+            except Exception, err:
+                print "failed to post to twitter", err.message
+                
             self.write( '{ "result": "authorized" }' )
         self.finish()
         #if paths[0] == 'frontdoor':
@@ -38,11 +55,37 @@ class PermHandler(tornado.web.RequestHandler):
         pass
 
 def make_app():
+
+    key_dir = "twitter_keys/"
+    with open(key_dir + "access-token.key","r") as f:
+        access_token_key = f.readline().strip()
+    with open(key_dir + "access-token-secret.key", "r") as f:
+        access_token_secret = f.readline().strip()
+    with open(key_dir + "consumer.key", "r") as f:
+        consumer_key = f.readline().strip()
+    with open(key_dir + "consumer-secret.key", "r") as f:
+        consumer_secret = f.readline().strip()
+
+    with open("twitter_pool.txt", "r") as f:
+        pool_lines = f.readlines()
+    pool = [line.strip() for line in pool_lines]
+    print pool
+		
+
+    api = twitter.Api(consumer_key = consumer_key,
+                      consumer_secret = consumer_secret,
+                      access_token_key = access_token_key,
+                      access_token_secret = access_token_secret)
+
     settings = {
         'debug': True,
         'static_path': os.path.join(PATH, 'static'),
         'template_path': os.path.join(PATH, 'templates'),
-        'ui_modules': uimodules
+        'ui_modules': uimodules,
+        'twitter_api': api,
+        'twitter_tweet_pool': pool,
+        'twitter_last_tweet': 0,
+        'twitter_cooldown_sec': 60*10
     }
 
     return tornado.web.Application([
@@ -54,5 +97,6 @@ if __name__ == "__main__":
     db = database.Database( 'makermon.db', database.db_schema )
     app = make_app()
     app.listen(8888)
-    print( "Makermon server started..." )
+    print( "Makermon server started...", time.time(), time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()) )
+    sys.stdout.flush()
     tornado.ioloop.IOLoop.current().start()
--- readme.md
+++ readme.md
@@ -5,6 +5,16 @@ This repository contains the code responsible for controlling access to various 
 ## Requirements
 * Python 2.7
 * Tornado `python -m pip install tornado`
+* python-twitter `pip install python-twitter`
+* twitter application keys
+
+##twitter application keys
+* create directory twitter_keys
+* store keys from apps.twitter.com
+* create twitter_keys/access-token.key
+* create twitter_keys/access-token-secret.key
+* create twitter_keys/consumer.key
+* create twitter_keys/consumer-secret.key
 
 ## Enclosure
 Link to custom 3D printed enclosure for door controller: http://a360.co/2HaIEuf  
